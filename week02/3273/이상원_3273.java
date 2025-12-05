8cf91d565a1abbcd2357155e84e55657fa41fd25
nippyclouding <wonderwall09in@naver.com>
2025-12-05 13:21:30 +0900

    이상원_3273

--- week02/3273/이상원_3273.java
+++ week02/3273/이상원_3273.java
@@ -58,3 +58,56 @@ class Main{
         return count;
     }
 }
+
+
+//2. 배열 풀이
+package main;
+
+import java.util.*;
+        import java.io.*;
+
+class Main{
+    static int n;
+    static int[] sequence;
+    static int x;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 입력
+        n = Integer.parseInt(br.readLine());
+        sequence = new int[n];
+
+        String secondLine = br.readLine();
+        StringTokenizer st = new StringTokenizer(secondLine);
+
+        int max = 0;
+
+        for(int i = 0; i < n; i++){
+            sequence[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, sequence[i]); // visited 배열의 크기를 산정하기 위해
+        }
+
+        x = Integer.parseInt(br.readLine());
+
+        int output = solution(max);
+
+        // 출력
+        bw.write(String.valueOf(output));
+        bw.flush();
+    }
+
+    static int solution(int max){
+        int count = 0;
+
+        boolean[] visited = new boolean[max + 1]; // 인덱스는 0부터 시작을 고려해서 +1
+
+        // 방문 처리
+        for(int temp : sequence){
+            if(visited[temp]) count++; // 이미 방문 처리되어있다면 ++
+            visited[x - temp] = true;
+        }
+        return count;
+    }
+}
