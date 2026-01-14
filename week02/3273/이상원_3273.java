95c19e60d957e7d08a9902a17d271e1ffa0d616f
nippyclouding <wonderwall09in@naver.com>
2026-01-14 19:36:08 +0900

    0114

--- week02/3273/이상원_3273.java
+++ week02/3273/이상원_3273.java
@@ -1,113 +1,53 @@
-package main;
-
-
-// 1. 투포인터 풀이
-import java.util.*;
+import java.uti.*;
 import java.io.*;
 
-class Main{
+class Main {
     static int n;
-    static int[] sequence;
-    static int x;
+    static int m;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] board;
+    static boolean[][] visited;
+    StringTokenizer st;
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static class Pair {
+        int x;
+        int y;
 
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         // 입력
-        n = Integer.parseInt(br.readLine());
-        sequence = new int[n];
-
-        String secondLine = br.readLine();
-        StringTokenizer st = new StringTokenizer(secondLine);
-        for(int i = 0; i < n; i++){
-            sequence[i] = Integer.parseInt(st.nextToken());
-        }
-
-        x = Integer.parseInt(br.readLine());
-
-        Arrays.sort(sequence);
-
-        int output = solution();
-
-        // 출력
-        bw.write(String.valueOf(output));
-        bw.flush();
-    }
-
-    static int solution(){
-        int count = 0;
-
-        int left = 0;
-        int right = sequence.length - 1;
-
-        while(left < right){
-
-            if(sequence[left] + sequence[right] == x){
-                count++;
-                left++;
-                right--;
-            } else if(sequence[left] + sequence[right] > x){
-                right--;
-            } else if(sequence[left] + sequence[right] < x){
-                left++;
-            }
-
-        }
-
-        return count;
-    }
-}
-
-
-//2. 배열 풀이
-package main;
-
-import java.util.*;
-        import java.io.*;
-
-class Main{
-    static int n;
-    static int[] sequence;
-    static int x;
-
-    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        // 입력
-        n = Integer.parseInt(br.readLine());
-        sequence = new int[n];
+        // BFS 세팅
+        board = new int[n][m];
+        visited = new boolean[n][m];
 
-        String secondLine = br.readLine();
-        StringTokenizer st = new StringTokenizer(secondLine);
+        Queue<Pair> q = new ArrayDeque<>();
+        q.offer(new Pair(0, 0,));
+        visited[0][0] = true;
 
-        int max = 0;
-
-        for(int i = 0; i < n; i++){
-            sequence[i] = Integer.parseInt(st.nextToken());
-            max = Math.max(max, sequence[i]); // visited 배열의 크기를 산정하기 위해
-        }
-
-        x = Integer.parseInt(br.readLine());
-
-        int output = solution(max);
-
+        int result = BFS(q);
         // 출력
-        bw.write(String.valueOf(output));
-        bw.flush();
+        System.out.println(result);
     }
 
-    static int solution(int max){
-        int count = 0;
+    private static int BFS(Queue<Pair> q) {
+        int result = 0;
 
-        boolean[] visited = new boolean[max + 1]; // 인덱스는 0부터 시작을 고려해서 +1
 
-        // 방문 처리
-        for(int temp : sequence){
-            if(visited[temp]) count++; // 이미 방문 처리되어있다면 ++
-            visited[x - temp] = true;
-        }
-        return count;
+
+
+        return result;
     }
+
+
 }
